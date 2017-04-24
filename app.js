e610810cb306a068d841e2b030e6db08cb6896e2
kaan bursa <kaanbursa9@gmail.com>
2017-04-24 23:45:21 +0300

    path dirname changed

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res)=>{
-	res.sendFile(path.join(__dirname, + 'public/index.html'))
+	res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(port, () =>{
